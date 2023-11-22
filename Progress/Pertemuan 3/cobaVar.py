8d978cad7f91c1c9cac8f7220ad26fa76330fa6c
Firzandi Ahsan Dwi Styana <90011948+firzandiads@users.noreply.github.com>
2023-11-23 00:22:36 +0700

    Delete Progress directory

--- Progress/Pertemuan 3/cobaVar.py
+++
@@ -1,7 +0,0 @@
-namaLengkap = "Firzandi Ahsan Dwi Styana"
-umur = 18
-tinggiBadan = 167
-
-print("Nama saya adalah " + namaLengkap)
-print("Umur saya adalah " + str(umur) + " tahun")
-print("Tinggi badan saya adalah " + str(tinggiBadan) + " cm")
--- Progress/Pertemuan 3/hello.py
+++
@@ -1,6 +0,0 @@
-"""
-Nama    : Firzandi Ahsan Dwi Styana
-NIM     : 21537144016
-"""
-# ini menampilkan tulisan sederhana
-print("Hello, World!")
